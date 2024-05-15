664a25d437a4621b9bf7ac9580e5ec7c492cd807
clatot <kat.clatot@gmail.com>
2024-05-15 14:52:45 +0200

    Modif page Home

--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -5,33 +5,35 @@ import Navigation from "../../components/Navigation/navigation";
 
 function Home() {
   return (
-    <div>
+    <>
       <Navigation />
-      <Banner />
-      <section className="features">
-        <Feature
-          imageSrc=".\src\assets\images\icon-chat.png"
-          title="You are our #1 priority"
-        >
-          Need to talk to a representative? You can get in touch through our
-          24/7 chat or through a phone call in less than 5 minutes.
-        </Feature>
-        <Feature
-          imageSrc=".\src\assets\images\icon-money.png"
-          title="More savings means higher rates"
-        >
-          The more you save with us, the higher your interest rate will be!
-        </Feature>
-        <Feature
-          imageSrc=".\src\assets\images\icon-security.png"
-          title="Security you can trust"
-        >
-          We use top of the line encryption to make sure your data and money is
-          always safe.
-        </Feature>
-      </section>
+      <main>
+        <Banner />
+        <section className="features">
+          <Feature
+            imageSrc=".\src\assets\images\icon-chat.png"
+            title="You are our #1 priority"
+          >
+            Need to talk to a representative? You can get in touch through our
+            24/7 chat or through a phone call in less than 5 minutes.
+          </Feature>
+          <Feature
+            imageSrc=".\src\assets\images\icon-money.png"
+            title="More savings means higher rates"
+          >
+            The more you save with us, the higher your interest rate will be!
+          </Feature>
+          <Feature
+            imageSrc=".\src\assets\images\icon-security.png"
+            title="Security you can trust"
+          >
+            We use top of the line encryption to make sure your data and money
+            is always safe.
+          </Feature>
+        </section>
+      </main>
       <Footer />
-    </div>
+    </>
   );
 }
 
