7fd9ea985197f70d7c4221172d1be16599e11420
clatot <kat.clatot@gmail.com>
2024-05-15 14:52:08 +0200

    Création Router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/home.jsx";
 import "./utils/main.css";
+import SignIn from "./pages/SignIn/signIn.jsx";
+import User from "./pages/User/user.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign" element={<SignIn />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
