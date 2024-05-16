22ecd29bbf2eca6f2d1e697847fea8bd60a642a2
clatot <kat.clatot@gmail.com>
2024-05-16 16:14:45 +0200

    Ajout Link Composant Nav

--- client/src/components/Navigation/navigation.jsx
+++ client/src/components/Navigation/navigation.jsx
@@ -1,19 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Navigation() {
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <Link className="main-nav-logo" to={"/"}>
         <img
           className="main-nav-logo-image"
           src="..\src\assets\images\argentBankLogo.png"
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
-        <a className="main-nav-item" href="./sign-in.html">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </a>
+        <Link className="main-nav-item" to={"/sign"}>
+          <i className="fa fa-user-circle"></i> Sign In
+        </Link>
       </div>
     </nav>
   );
