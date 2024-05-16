f86b5f9d44f9a347db290465628be8a95d75f2d2
clatot <kat.clatot@gmail.com>
2024-05-16 16:14:55 +0200

    Création composant form

--- client/src/pages/SignIn/signIn.jsx
+++ client/src/pages/SignIn/signIn.jsx
@@ -1,4 +1,5 @@
 import Footer from "../../components/Footer/footer";
+import Form from "../../components/Form/form";
 import Navigation from "../../components/Navigation/navigation";
 
 function SignIn() {
@@ -8,23 +9,9 @@ function SignIn() {
       <main className="page-container">
         <div className="main bg-dark">
           <section className="sign-in-content">
-            <i className="fa fa-user-circle sign-in-icon"></i>
+            <i className="fa fa-user-circle"></i>
             <h1>Sign In</h1>
-            <form>
-              <div className="input-wrapper">
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" />
-              </div>
-              <div className="input-wrapper">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
-              </div>
-              <div className="input-remember">
-                <input type="checkbox" id="remember-me" />
-                <label htmlFor="remember-me">Remember me</label>
-              </div>
-              <button className="sign-in-button">Sign In</button>
-            </form>
+            <Form />
           </section>
         </div>
       </main>
