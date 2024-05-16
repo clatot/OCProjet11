5de9bb0a01e08082fb5048e6731fe05e1a9aab83
clatot <kat.clatot@gmail.com>
2024-05-16 16:14:09 +0200

    Ajout composant Feature

--- client/src/components/Feature/feature.jsx
+++ client/src/components/Feature/feature.jsx
@@ -1,9 +1,9 @@
-function Feature({ imageSrc, title, children }) {
+function Feature({ imageSrc, title, content }) {
   return (
     <div className="feature-item">
       <img src={imageSrc} alt="Chat Icon" className="feature-icon" />
       <h3 className="feature-item-title">{title}</h3>
-      <p>{children}</p>
+      <p>{content}</p>
     </div>
   );
 }
--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -4,32 +4,44 @@ import Footer from "../../components/Footer/footer";
 import Navigation from "../../components/Navigation/navigation";
 
 function Home() {
+  const featureContent = [
+    {
+      id: 1,
+      image: "./src/assets/images/icon-chat.png",
+      title: "You are our #1 priority",
+      content:
+        "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
+    },
+    {
+      id: 2,
+      image: "./src/assets/images/icon-money.png",
+      title: "More savings means higher rates",
+      content:
+        "The more you save with us, the higher your interest rate will be!",
+    },
+    {
+      id: 3,
+      image: "./src/assets/images/icon-security.png",
+      title: "Security you can trust",
+      content:
+        "We use top of the line encryption to make sure your data and money is always safe.",
+    },
+  ];
+
   return (
     <>
       <Navigation />
       <main>
         <Banner />
         <section className="features">
-          <Feature
-            imageSrc=".\src\assets\images\icon-chat.png"
-            title="You are our #1 priority"
-          >
-            Need to talk to a representative? You can get in touch through our
-            24/7 chat or through a phone call in less than 5 minutes.
-          </Feature>
-          <Feature
-            imageSrc=".\src\assets\images\icon-money.png"
-            title="More savings means higher rates"
-          >
-            The more you save with us, the higher your interest rate will be!
-          </Feature>
-          <Feature
-            imageSrc=".\src\assets\images\icon-security.png"
-            title="Security you can trust"
-          >
-            We use top of the line encryption to make sure your data and money
-            is always safe.
-          </Feature>
+          {featureContent.map((feature) => (
+            <Feature
+              key={feature.id}
+              imageSrc={feature.image}
+              title={feature.title}
+              content={feature.content}
+            />
+          ))}
         </section>
       </main>
       <Footer />
