fa25a7180d03e8ec966dab8192569cf80ce1be2e
clatot <kat.clatot@gmail.com>
2024-05-27 11:31:57 +0200

    Ajout componant form

--- client/src/components/Form/form.jsx
+++ client/src/components/Form/form.jsx
@@ -1,19 +1,60 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginPost } from "../../Store/loginSlice";
+import { profilePost } from "../../Store/profileSlice";
+import { useNavigate } from "react-router-dom";
+
 function Form() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { loading, token } = useSelector((state) => state.login);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginEvent = (e) => {
+    e.preventDefault();
+
+    let loginData = {
+      email,
+      password,
+    };
+    dispatch(loginPost(loginData));
+  };
+
+  useEffect(() => {
+    if (token) {
+      dispatch(profilePost(token));
+      navigate("/user");
+    }
+  }, [token]);
+
   return (
-    <form>
+    <form onSubmit={handleLoginEvent}>
       <div className="input-wrapper">
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input
+          type="text"
+          id="username"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="input-wrapper">
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <div className="input-remember">
         <input type="checkbox" id="remember-me" />
         <label htmlFor="remember-me">Remember me</label>
       </div>
-      <button className="sign-in-button">Sign In</button>
+      <button className="sign-in-button" disabled={loading}>
+        Sign In
+      </button>
     </form>
   );
 }
