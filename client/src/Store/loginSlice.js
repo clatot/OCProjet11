608b6c1d34bb37ea7c618a6cf01d52dad78e0778
clatot <kat.clatot@gmail.com>
2024-05-27 11:31:28 +0200

    Ajout login Slice

---
+++ client/src/Store/loginSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API_URL = "http://localhost:3001/api/v1";
+
+const loginSlice = createSlice({
+  name: "login",
+  initialState: {
+    token: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    loginStart: (state) => {
+      state.loading = true;
+      state.error = null;
+      console.log("Start");
+    },
+    loginSuccess: (state, action) => {
+      state.loading = false;
+      state.token = action.payload.body.token;
+      console.log("Success");
+    },
+    loginFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+      console.log("Fail");
+    },
+    logout: (state) => {
+      state.token = null;
+      console.log("logout");
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailure, logout } =
+  loginSlice.actions;
+export default loginSlice.reducer;
+
+export const loginPost = (loginData) => async (dispatch) => {
+  dispatch(loginStart());
+  try {
+    const response = await axios.post(`${API_URL}/user/login`, loginData);
+    dispatch(loginSuccess(response.data));
+  } catch (error) {
+    dispatch(loginFailure(error.response.data));
+  }
+};
